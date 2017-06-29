3f677242c34da39f2936601e3c7f66b64e12e7af
Luc Boutier <luc@fastconnect.fr>
2017-06-29 10:01:24 +0200

    Add lifecycle test

--- src/test/java/org/alien4cloud/it/run/RunCloudify4AmazonIT.java
+++ src/test/java/org/alien4cloud/it/run/RunCloudify4AmazonIT.java
@@ -16,9 +16,9 @@ import cucumber.api.junit.Cucumber;
         //
         "classpath:features/cloudify4/00_aws_setup.feature", // Configure cfy orchestrator and aws location
         "classpath:features/common/00__setup.feature", // Import test archives
-        // "classpath:features/location/01_compute.feature", // Test linux compute support
-        // "classpath:features/location/02_compute_deletable_block.feature", // Test linux compute and deletable block storage support
-        // "classpath:features/location/03_compute_block.feature", // Test linux compute and block storage support
+        "classpath:features/location/01_compute.feature", // Test linux compute support
+        "classpath:features/location/02_compute_deletable_block.feature", // Test linux compute and deletable block storage support
+        "classpath:features/location/03_compute_block.feature", // Test linux compute and block storage support
         "classpath:features/orchestrator/20_lifecycle.feature" // Check that orchestrator execute correct lifecycle
         //
 }, format = { "pretty", "html:target/cucumber/cloudify3/amazon", "json:target/cucumber/cloudify3/cucumber-amazon.json" }, glue = { "alien4cloud.it",
